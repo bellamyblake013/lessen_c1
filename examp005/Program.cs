aa7654b4d4a7ca839cbfb378b43e9ed86da6f489
Bellamy <Spa1k199991@gmail.com>
2023-05-27 15:34:13 +0300

    project7

---
+++ examp005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "александр")
+{
+    Console.WriteLine("Ура это АЛЕКСАНДР!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
